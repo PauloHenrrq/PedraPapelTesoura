f0a613224f91d681d81b9f79ad5e8e8d8f17b3b9
PAULO HENRIQUE DE ARAUJO RODRIGUES <PAULO0855@DIGITALCOLLEGE.LOCAL>
2024-07-09 15:35:05 -0300

    JOKEMPO finalizado

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,85 @@
 const opcoes = {
-    1 : { nome: 'pedra', img: 'IMAGENS/pedra.png', pedra: 'EMPATE', papel: 'PERDEU', tesoura: 'GANHOU'},
-    2 : { nome: 'papel', img: 'IMAGENS/papel.png', pedra: 'GANHOU', papel: 'EMPATE', tesoura: 'PERDEU'},
-    3 : { nome: 'tesoura', img: 'IMAGENS/tesoura.png', pedra: 'PERDEU', papel: 'GANHOU', tesoura: 'EMPATE'}
+    1 : { nome: 'pedra', img:'img/pedra.png', pedra: 'EMPATE', papel : 'PERDEU', tesoura: 'GANHOU'},
+    2 : { nome: 'papel', img:'img/papel.png', pedra: 'GANHOU', papel : 'EMPATE', tesoura: 'PERDEU'},
+    3 : { nome: 'tesoura', img:'img/tesoura.png', pedra: 'PERDEU', papel : 'GANHOU', tesoura: 'EMPATE'}
 }
 
-let escolhaJogador = {}
-let escolhaComputador = {}
+//seleção dos objetos do document
+//imagem e texto da escolha do usuário
+let imgJogador = document.getElementById('imgUser')
+let pJogador =  document.getElementById('escolhaUsuario')
 
-function escolherJogada(num) {
-    escolhaJogador = opcoes[num]
+//imagem e texto da escolha do computador
+let imgComputador = document.getElementById('imgComput')
+let pComputador =  document.getElementById('escolhaComputador')
+
+//resultado final
+let resultadoFinal = document.getElementById('resultado')
+
+//contagem dos pontos no document
+let contagemPontosComputador = document.getElementById('pontosComputador')
+let contagemPontosJogador = document.getElementById('pontosUsuario')
+
+//variáveis globais para escolha do usuário e do computador
+let escolhaJogador= {}
+let escolhaComputador= {}
+
+//variáveis globais para pontuação dos jogadores
+let pontosJogador = 0
+let pontosComputador = 0
+
+function escolherJogada(num){
+    return opcoes[num]
 }
 
-let numComputador = math.random((math.random()*3)+1)
-escolhaComputador = opcoes[numComputador]
+function montarJogadas(numJogador){
+    let numComp = Math.floor((Math.random()*3)+1)
+    escolhaJogador = escolherJogada(numJogador)
+    escolhaComputador = escolherJogada(numComp)
+}
 
+function avaliarGanhador(){
+
+    if( escolhaJogador[escolhaComputador.nome] == 'GANHOU'){
+        pontosJogador += 1
+    }
+    else if (escolhaJogador[escolhaComputador.nome] == 'PERDEU'){
+        pontosComputador += 1 
+    }
+}
+
+function montarTela(){
+    imgJogador.src= escolhaJogador.img
+    imgComputador.src = escolhaComputador.img
+
+    imgJogador.alt= escolhaJogador.nome
+    imgComputador.alt = escolhaComputador.nome
+
+    pJogador.innerHTML = ` Você escolheu ${escolhaJogador.nome}`
+    pComputador.innerHTML = `Computador escolheu ${escolhaComputador.nome}`
+
+    contagemPontosComputador.innerHTML = pontosComputador
+    contagemPontosJogador.innerHTML = pontosJogador
+
+    resultadoFinal.innerHTML= escolhaJogador[escolhaComputador.nome]
+
+    //adicção da classe para manipulação do style
+    switch(escolhaJogador[escolhaComputador.nome]){
+        case 'GANHOU':
+            resultadoFinal.classList = 'ganhou'
+            break
+        case 'PERDEU':
+            resultadoFinal.classList = 'perdeu'
+            break
+        default:
+            resultadoFinal.classList = 'empatou'
+            break
+    }
+
+}
+
+function jogar(num){
+    montarJogadas(num)
+    avaliarGanhador()
+    montarTela()
+}
